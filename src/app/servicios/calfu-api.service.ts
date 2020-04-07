af72c66de689eb8c5043ef3ad89b2d6de3b47209
CristobalLopez <clopezosses@gmail.com>
2020-04-07 17:29:07 -0400

    creacion servicio api calfu

---
+++ src/app/servicios/calfu-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ComentarioModel } from '../models/comentario.model';
+import { MainConfigModel } from '../models/mainConfig.model';
+import { MeGustaModel } from '../models/meGusta.model';
+import { ObraMstrModel } from '../models/obraMstr.model';
+import { TipoObraModel } from '../models/tipoObra.model';
+import { UserMstrModel } from '../models/userMstr.model';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalfuApiService {
+  private url ='http://localhost:60538/api/';
+
+  constructor(private http: HttpClient) { 
+    
+  }
+  
+}
